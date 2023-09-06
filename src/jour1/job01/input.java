89937b6edadc37ca3f2b39dce53c700a16833609
joris-landaret <joris.landaret@laplateforme.io>
2023-09-06 11:26:30 +0200

    finish job01

--- src/jour1/job01/input.java
+++ src/jour1/job01/input.java
@@ -1,5 +1,15 @@
 package jour1.job01;
 
-public class input {
+import java.util.Scanner;
 
+public class input {
+    public static void main(String[] args) {
+        System.out.println("java HelloWorld");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Veuillez entrer votre nom : ");
+        String userName = scanner.nextLine();
+        System.out.println("Hello, " + userName + "!");
+    }
 }
