dec8eb705e076f7e514279bea3deaa8bf34e6042
joris-landaret <joris.landaret@laplateforme.io>
2023-09-11 09:12:03 +0200

    jour3 job05 finis

---
+++ src/jour3/job05/StreamOperation.java
@@ -0,0 +1,21 @@
+package jour3.job05;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+public class StreamOperation {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(2);
+        numbers.add(4);
+        numbers.add(6);
+        numbers.add(3);
+        numbers.add(8);
+
+        ArrayList<Integer> resultat = (ArrayList<Integer>) numbers.stream()
+                .map(nombre -> nombre * 2) // Double chaque nombre
+                .filter(nombre -> nombre > 10) // Filtre les nombres supérieurs à 10
+                .collect(Collectors.toList()); // Collecte les résultats dans une liste
+
+        System.out.println("Résultat : " + resultat);
+    }
+}
